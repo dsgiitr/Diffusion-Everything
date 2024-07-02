ecca0e223ca84e029ca109124e38407c4be16638
Parth-Badgujar <parthbadgujar57@gmail.com>
2024-07-03 00:45:27 +0530

    add fast inference

--- ddpm/ddpm.py
+++ ddpm/ddpm.py
@@ -130,6 +130,30 @@ class DDPM():
                 streamlit_callback(epsilon_theta, mean, old_x_T, self.tensor2numpy(x_T.cpu()), t)
             x_Ts.append(self.tensor2numpy(x_T.cpu()))
         return x_Ts
+    
+    @torch.inference_mode()
+    def fast_generate(self, numImages, labels, steps, lerp = 1, streamlit_callback = None):
+        ns = self.noise_scheduler
+        alpha_cumprod = ns.alpha_cumprod[torch.linspace(0, self.timesteps - 1, steps, device = self.device).long()]
+        alpha_cumprod_minus_one = torch.cat((torch.tensor([1], device = self.device), alpha_cumprod[:-1]), dim = -1)
+        beta = 1 - alpha_cumprod / alpha_cumprod_minus_one 
+        alpha = 1 - beta
+        beta_tilda = ((1 - alpha_cumprod_minus_one) / (1 - alpha_cumprod)) * beta 
+        beta_final = beta * (1 - lerp) + beta_tilda * lerp
+        sigma = beta_final.sqrt()
+        idx = torch.linspace(0, self.timesteps - 1, steps, device = self.device).long()
+        x_Ts = []
+        x_T = torch.randn(numImages, *self.img_shape, device = self.device) 
+        x_Ts.append(self.tensor2numpy(x_T))
+        for t in tqdm(torch.arange(steps - 1, 0, -1, device = self.device)):
+            epsilon_theta = self.UNet(x_T, idx[t], labels).sample 
+            mean = (1 / alpha[t].sqrt()) * (x_T - ((1 - alpha[t])/(1 - alpha_cumprod[t]).sqrt()) * epsilon_theta) 
+            old_x_T = x_T
+            x_T = self.sample(mean, sigma[t])
+            if streamlit_callback :
+                streamlit_callback(epsilon_theta, mean, old_x_T, self.tensor2numpy(x_T.cpu()), t)
+            x_Ts.append(self.tensor2numpy(x_T.cpu()))
+        return x_Ts 
 
     @staticmethod
     def sample(mean, std):
